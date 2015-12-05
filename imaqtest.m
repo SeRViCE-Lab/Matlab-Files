c9471d44cdd127758be93ae1c864e8caadd1b2cd
lakehanne <patlekano@gmail.com>
2015-12-05 15:20:57 -0600

    all

--- imaqtest.m
+++
@@ -1,7 +0,0 @@
-flag.Visualize = true;
-
-%cleanupWorkspace
-
-obj.colorVideoDevice = imaq.VideoDevice('kinect', 1);
-set(obj.colorVideoDevice, 'ReturnDataTyoe', 'uint8');
-obj.depthVideoDevice = imaq.VideoDevice('kinect', 2);
